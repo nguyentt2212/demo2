a33b3ec66e015fb8a715e2cc341888bb63fd1157
nguyentt2212 <nguyenttp02595@fpt.edu.vn>
2015-11-23 22:15:31 +0700

    34

--- WindowsApplication1/WindowsApplication1/demo.vb
+++ WindowsApplication1/WindowsApplication1/demo.vb
@@ -3,4 +3,9 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Close()
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("ok")
+
+    End Sub
 End Class
